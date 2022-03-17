26f345142ff39d1da3854cd1655cb4d7efefcd1e
gitpodlinovamaryna <gitpodlinovamaryna@gmail.com>
2022-03-17 18:51:17 +0200

    Add test-script backups

--- Scripts/backup.sh
+++ Scripts/backup.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
-
+PROJ=$[ $1 ]
 NOW_DATE=$(date +'%Y-%m-%d-%H-%M-%S')
 
+BACKUP_DIR="BackUps"
+BACKUP_DIR_PATH=${HOME}/Documents/${BACKUP_DIR}
+    if [ ! -d "$BACKUP_DIR_PATH" ];
+        then mkdir $BACKUP_DIR_PATH
+        fi
+        
+        ARCHIVE_NAME="${PROJ}_${NOW_DATE}--cc.tar.gz"
+        
