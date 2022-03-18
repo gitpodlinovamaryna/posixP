2ad832d03a1c6bf5e6e3aea40009b2b868a89fe5
gitpodlinovamaryna <gitpodlinovamaryna@gmail.com>
2022-03-18 12:16:02 +0200

    Change backup script(creating backup folder, creating archive and put in backup folder

--- Scripts/backup.sh
+++ Scripts/backup.sh
@@ -4,11 +4,13 @@ PROJ=$1
 PROJ_NAME=`basename "$1"`
 NOW_DATE=$(date +'%Y-%m-%d-%H-%M-%S')
 BACKUP_DIR_NAME="BackUps"
-BACKUP_DIR_PATH=${HOME}/Documents/$BACKUP_DIR
+BACKUP_DIR_PATH=${HOME}/Documents/${BACKUP_DIR_NAME}
 echo $BACKUP_DIR_PATH
     if [ ! -d "$BACKUP_DIR_PATH" ];
         then mkdir $BACKUP_DIR_PATH
         fi
-        cd $BACKUP_DIR_PATH
+        cd ${HOME}
+        cd Documents
+        cd $BACKUP_DIR_NAME
         ARCHIVE_NAME="${PROJ_NAME}_${NOW_DATE}.tar.gz"
         tar -cvzf $ARCHIVE_NAME $PROJ_NAME
