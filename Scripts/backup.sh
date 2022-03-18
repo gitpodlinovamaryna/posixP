1c18e12b28689c655b06806de87ac0b3dbd21bcf
gitpodlinovamaryna <gitpodlinovamaryna@gmail.com>
2022-03-18 02:54:20 +0200

    archive-test1

--- Scripts/backup.sh
+++ Scripts/backup.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
-PROJ=$[ $1 ]
-NOW_DATE=$(date +'%Y-%m-%d-%H-%M-%S')
 
-BACKUP_DIR="BackUps"
-BACKUP_DIR_PATH=${HOME}/Documents/${BACKUP_DIR}
+PROJ=$1
+PROJ_NAME=`basename "$1"`
+NOW_DATE=$(date +'%Y-%m-%d-%H-%M-%S')
+BACKUP_DIR_NAME="BackUps"
+BACKUP_DIR_PATH=${HOME}/Documents/$BACKUP_DIR
+echo $BACKUP_DIR_PATH
     if [ ! -d "$BACKUP_DIR_PATH" ];
         then mkdir $BACKUP_DIR_PATH
         fi
-        
-        ARCHIVE_NAME="${PROJ}_${NOW_DATE}--cc.tar.gz"
-        
+        cd $BACKUP_DIR_PATH
+        ARCHIVE_NAME="${PROJ_NAME}_${NOW_DATE}.tar.gz"
+        tar -cvzf $ARCHIVE_NAME $1
