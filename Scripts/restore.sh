b0d56c040d33825c15a2c88ebde077a30fe34ab6
gitpodlinovamaryna <gitpodlinovamaryna@gmail.com>
2022-03-19 00:52:48 +0200

    Add restored script

--- Scripts/restore.sh
+++ Scripts/restore.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
 ARCHIVE_FULL=$1
-ARCHIVE_NAME=$(cut -d "=" -f2- <<< $1)
+ARCHIVE_NAME=`basename "$1"`
 echo $ARCHIVE_NAME
-
+RESTORE_DIR_NAME="Restored"
+RESTORE_DIR_PATH=${HOME}/Documents/${RESTORE_DIR_NAME}
+echo $RESTORE_DIR_PATH
+    if [ ! -d "$RESTORE_DIR_PATH" ];
+        then mkdir $RESTORE_DIR_PATH
+        fi
+cd $RESTORE_DIR_PATH
+tar -xf $1
