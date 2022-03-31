bf560cd4f263f20d84b3bc11fd848590a9051382
gitpodlinovamaryna <gitpodlinovamaryna@gmail.com>
2022-03-31 14:11:28 +0300

    modified .gitignore

--- progC/main.c
+++ progC/main.c
@@ -1,36 +1,28 @@
-
-
-
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{   
+int main(int argc, char *argv[]){   
     FILE * file;
+    putenv("MY_VAR=This is My_VAR: ");
     char* my_var  = getenv("MY_VAR");
     if(!my_var){
-       return 123;
+        fprintf( stderr, "Error 123\n" );
+        return 123;
     }
-    else 
-    {
-        if (argc > 1)
-        {
-            file = fopen("env.txt", "a");
-            if(file)
-            {
+    else{
+        if (argc > 1){
+            file = fopen(argv[argc-1], "a");
+            if(file){
                 if(!fputs(my_var, file))
-                fclose(file);
+                    fclose(file);
                 else
                     return 1;
             }
             else
-            {
                 return 1;
         }
         else
-        {
-            printf("Var found: %s", my_var);
-        }
+            fprintf( stdout, "Var found: %s", my_var );
     }
     return 0;
 }
